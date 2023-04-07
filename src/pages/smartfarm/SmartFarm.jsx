d8b03242d705d6a415c72afca6a0b0707a28485d
vsidera <siderravictor@gmail.com>
2023-04-07 12:39:53 +0300

    complete the bulbs

--- src/pages/smartfarm/SmartFarm.jsx
+++ src/pages/smartfarm/SmartFarm.jsx
@@ -22,6 +22,10 @@ const SmartFarm = () => {
   const [eventMessage, setEventMessage] = useState('');
   const [eventTitle, setEventTitle] = useState('');
 
+  const [auto, setAuto] = useState(false)
+
+  const [pumpMode, setPumpMode] = useState("0")
+
   const [activeTab, setActiveTab] = useState(0);
 
   const [pumps, setPumps] = useState( [
@@ -29,7 +33,12 @@ const SmartFarm = () => {
   
   ]);
 
+
   const [pumpsAuto, setPumpsAuto] = useState([])
+
+  console.log("PUMPS ARE!!!!!",pumpsAuto)
+
+  
 
   const [valves, setValves] = useState(
      [
@@ -74,11 +83,23 @@ const SmartFarm = () => {
           const pumps = res.data.filter(obj => ['p_s'].includes(obj.subtopic));
           const pumpsAuto = res.data.filter(obj => ['p_m'].includes(obj.subtopic));
 
-          console.log("PUMPS MODE!!!!", pumps)
+          
+          // console.log("PUMPS MODE!!!!", pumps)
           setValves(valvesManual);
           setValvesAuto(valvesAuto)
           setPumps(pumps)
           setPumpsAuto(pumpsAuto)
+          if (pumpsAuto && pumpsAuto[0] && pumpsAuto[0].value==="1"){
+            console.log("ITS ON AUTO!!!!!", pumpsAuto[0].value)
+            setPumpMode("1")
+            pumps[0]["mode"] = "1";
+            setAuto(true)
+          }
+          else{
+            console.log("ITS ON MANUAL!!!!!")
+            setAuto(false)
+            pumps[0]["mode"] = "0";
+          }
         }
       })
       .catch((err) => {
@@ -93,7 +114,7 @@ const SmartFarm = () => {
   }, []);
 
 
-
+ console.log("auto is!!!!!", pumps)
   
 
   const handleValveStatusChange = (id) => (event) => {
@@ -175,6 +196,45 @@ const SmartFarm = () => {
     }
   };
 
+  const handleModeChangeAndSwitchPump = (subtopic) => (event) => {
+    const isChecked = event.target.checked;
+    const newMode = isChecked ? "1" : "0";
+    const pump = pumps.find((pump) => pump.subtopic === subtopic);
+
+    console.log("PUMP MODE", pumpMode, newMode)
+  
+    if (pump.mode !== newMode) {
+      setPumps(
+        pumps.map((b) =>
+          b.subtopic === subtopic ? { ...b, mode: newMode } : b
+        )
+      );
+  
+      const cmdBody = {
+        imei: imei,
+        command: "p_m",
+        value: newMode.toString(),
+      };
+      sendCommand(cmdBody)
+        .then((res) => {
+          if (res.status === 200) {
+            setEventType("success");
+            setEventMessage("Command Successfully Sent");
+            setEventTitle("SEND COMMAND");
+            setIsSnackBarAlertOpen(true);
+          } else {
+            setEventType("fail");
+            setEventMessage("COMMAND NOT SENT");
+            setEventTitle("SEND COMMAND");
+            setIsSnackBarAlertOpen(true);
+          }
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+       
+  
+
   const handleStatusChangeAndSwitchPumpAuto = (subtopic) => (event) => {
     const isChecked = event.target.checked;
     const newStatus = isChecked ? "ON" : "OFF";
@@ -237,56 +297,71 @@ const SmartFarm = () => {
         </Tabs>
         {activeTab === 0 && (
           <>
-      <Accordion defaultExpanded={true}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="pumps-content"
-          id="pumps-header"
-        >
-          <span className="text-lg font-medium"> PUMPS</span>
-         
-        </AccordionSummary>
- 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-1">
-                  {pumps.map((pump) => (
-                    <div key={pump.id} className="bg-gray-100 p-4 rounded-lg">
-                      <h4 className="text-lg font-normal mb-2">{pump.device_imei}</h4>
-                      <div className="flex items-center">
-                        <p className="mr-4">
-                          Subtopic: <span className="font-medium">{pump.subtopic}</span>
-                        </p>
-                        <div className="flex items-center">
-                        {/* <p className="mr-4">
-                          Mode: <span className="font-medium">{pump.subtopic}</span>
-                        </p> */}
-                        <div className="flex items-center">
-                      <p className="mr-4">
-                        Status:{" "}
-                        <span
-                          className={
-                            pump.value === "1"
-                              ? "text-green-500 font-medium"
-                              : "text-red-500 font-medium"
-                          }
-                        >
-                          {pump.value ==="1" ? "ON" : "OFF"}
-                          {/* {pump.value} */}
-                        </span>
-                      </p>
-                      <Switch
-                        checked={pump.value === "1"}
-                        onChange={handleStatusChangeAndSwitchPump(pump.subtopic)}
-                        color="primary"
-                        inputProps={{ "aria-label": "toggle pump status" }}
-                      />
-                    </div>
-                      </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
+   <Accordion defaultExpanded={true}>
+  <AccordionSummary
+    expandIcon={<ExpandMoreIcon />}
+    aria-controls="pumps-content"
+    id="pumps-header"
+  >
+    <span className="text-lg font-medium"> PUMPS</span>
+  </AccordionSummary>
 
-      </Accordion>
+  {auto === false && (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-1">
+      {pumps &&
+        pumps.map((pump) => (
+          <div key={pump.id} className="bg-gray-100 p-4 rounded-lg">
+          <h4 className="text-lg font-normal mb-2">{pump.device_imei}</h4>
+          <p className="mr-4">
+              Subtopic:{" "}
+              <span className="font-medium">{pump.subtopic}</span>
+            </p>
+          <div className="flex items-center">
+            
+            <div className="flex items-center">
+              
+              <div className="flex items-center">
+                <p className="mr-4">
+                  Status:{" "}
+                  <span
+                    className={
+                      pump.value === "1"
+                        ? "text-green-500 font-medium"
+                        : "text-red-500 font-medium"
+                    }
+                  >
+                    {pump.value === "1" ? "ON" : "OFF"}
+                  </span>
+                </p>
+                <Switch
+                  checked={pump.value === "1"}
+                  onChange={handleStatusChangeAndSwitchPump(pump.subtopic)}
+                  color="primary"
+                  inputProps={{ "aria-label": "toggle pump status" }}
+                />
+              </div>
+              <div className="flex items-center">
+              <p className="mr-4">
+                Mode: <span className="font-medium">{pump.mode === "1" ? "Auto" : "Manual"}</span>
+              </p>
+                <Switch
+                  checked={pump.mode === "1"}
+                  onChange={handleModeChangeAndSwitchPump(pump.subtopic)}
+                  color="primary"
+                  inputProps={{ "aria-label": "toggle pump mode" }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        ))}
+    </div>
+  )}
+
+  {auto === true && <div>Pump in Auto</div>}
+</Accordion>
+
       <Accordion >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
@@ -344,44 +419,62 @@ const SmartFarm = () => {
          
         </AccordionSummary>
  
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-1">
-        {pumpsAuto.map((pump) => (
-                    <div key={pump.id} className="bg-gray-100 p-4 rounded-lg">
-                      <h4 className="text-lg font-normal mb-2">{pump.device_imei}</h4>
-                      <div className="flex items-center">
-                        <p className="mr-4">
-                          Subtopic: <span className="font-medium">{pump.subtopic}</span>
-                        </p>
-                        <div className="flex items-center">
-                        {/* <p className="mr-4">
-                          Mode: <span className="font-medium">{pump.subtopic}</span>
-                        </p> */}
-                        <div className="flex items-center">
-                      <p className="mr-4">
-                        Status:{" "}
-                        <span
-                          className={
-                            pump.value === "1"
-                              ? "text-green-500 font-medium"
-                              : "text-red-500 font-medium"
-                          }
-                        >
-                          {pump.value ==="1" ? "ON" : "OFF"}
-                          {/* {pump.value} */}
-                        </span>
-                      </p>
-                      <Switch
-                        checked={pump.value === "1"}
-                        onChange={handleStatusChangeAndSwitchPumpAuto(pump.subtopic)}
-                        color="primary"
-                        inputProps={{ "aria-label": "toggle pump status" }}
-                      />
-                    </div>
-                      </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
+        {auto === true && (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-1">
+    {pumps &&
+      pumps.map((pump) => (
+        <div key={pump.id} className="bg-gray-100 p-4 rounded-lg">
+        <h4 className="text-lg font-normal mb-2">{pump.device_imei}</h4>
+        <p className="mr-4">
+            Subtopic:{" "}
+            <span className="font-medium">{pump.subtopic}</span>
+          </p>
+        <div className="flex items-center">
+          
+          <div className="flex items-center">
+            
+            <div className="flex items-center">
+              <p className="mr-4">
+                Status:{" "}
+                <span
+                  className={
+                    pump.value === "1"
+                      ? "text-green-500 font-medium"
+                      : "text-red-500 font-medium"
+                  }
+                >
+                  {pump.value === "1" ? "ON" : "OFF"}
+                </span>
+              </p>
+              <Switch
+                checked={pump.value === "1"}
+                onChange={handleStatusChangeAndSwitchPump(pump.subtopic)}
+                color="primary"
+                inputProps={{ "aria-label": "toggle pump status" }}
+                disabled={true}
+              />
+            </div>
+            <div className="flex items-center">
+            <p className="mr-4">
+              Mode: <span className="font-medium">{pump.mode === "1" ? "Auto" : "Manual"}</span>
+            </p>
+              <Switch
+                checked={pump.mode === "1"}
+                onChange={handleModeChangeAndSwitchPump(pump.subtopic)}
+                color="primary"
+                inputProps={{ "aria-label": "toggle pump mode" }}
+                
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      ))}
+  </div>
+  )}
+
+  {auto === false && <div>Pump in Manual</div>}
 
       </Accordion >
       <Accordion>
