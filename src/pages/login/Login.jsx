ed39119f585b16fafdf7539e2fab6dee3877d47a
vsidera <siderravictor@gmail.com>
2023-04-11 13:02:04 +0300

    set index page as login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -17,21 +17,13 @@ const Login = () => {
   
   const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-  // Init user state
-  const [user, setUser] = useState(null);
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
-
-  const logout = () => {
-    setUser(null);
-    cookies.remove("jwt_authorization");
-  };
 
   const greenButton = {
     backgroundColor: "green",
     color: "white",
   };
-
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -54,54 +46,31 @@ const Login = () => {
           setIsSnackBarAlertOpen(true);
           setTimeout(() => {
             const priviledge = localStorage.getItem('type')
-            console.log(typeof priviledge); 
-            console.log("THE PRIV IS!!!!!!","ADMIN")
             if (priviledge === '"ADMIN"'){
               console.log("THEY ARE EQUAL!!!!!!")
               navigate("/devices");
             }
             else{
-              console.log("THEY ARE NOT EQUAL!!!!!!")
               navigate("/my-devices");
-
             }
             // props.history.push('/sidebar');
           }, 1000);
         }
-        // Schedule a logout function to run after one hour
-        const logoutTimer = setTimeout(function () {
-          // Remove the token and redirect the user to the login page
-          localStorage.removeItem("token");
-          localStorage.removeItem("loginTime");
-          window.location.href = "/login";
-        }, 60 * 60 * 1000); // one hour in milliseconds
-
-        // Clear the logout timer if the user logs out manually
-        // function logout() {
-        //   clearTimeout(logoutTimer);
-        //   localStorage.removeItem("key");
-        //   localStorage.removeItem("loginTime");
-        //   window.location.href = "/login";
-        // }
       })
       .catch((err) => {
-        // setIsError(true);
-        // setErrorMsg('Network error');
+       
       });
   };
 
   const login = (jwt_token) => {
     // Decode Token
     const decoded = jwt(jwt_token);
-
     // Set user state
     cookies.set("jwt_authorization", jwt_token, {
       expires: new Date(decoded.exp * 1000),
     });
   };
-
   const logoUrl = `${process.env.PUBLIC_URL}/images/irri.jpeg`;
-
 
   return (
     <>
@@ -112,78 +81,80 @@ const Login = () => {
         handleClose={() => setIsSnackBarAlertOpen(false)}
         title={eventTitle}
       />
-    <section class="bg-gray-50 dark:bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div class="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <div class="flex items-center justify-center mb-4">
+<section class="bg-gray-50 dark:bg-gray-900 flex justify-center items-center h-screen">
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto">
+    <div class="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+
+      <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <div class="flex items-center justify-center mb-4">
           <img src={logoUrl} alt="Logo" />
+        </div>
+
+        <form class="space-y-4 md:space-y-6" action="#">
+          <div>
+            <label
+              for="email"
+              class="block mb-2 text-lg font-light text-black dark:text-white text-center"
+            >
+              Mobile Number
+            </label>
+            <input
+              type="email"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              name="email"
+              id="email"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder-center"
+              placeholder="254711438911"
+              required=""
+            />
+          </div>
+          <div>
+            <label
+              for="password"
+              class="block mb-2 text-lg font-light text-gray-900 dark:text-white text-center"
+            >
+              Pin
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              id="password"
+              placeholder="••••"
+              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required=""
+            />
+          </div>
+          <div class="flex items-center justify-between flex-wrap">
+            <div class="flex items-start">
+              <div class="flex items-center h-5">
+                <input
+                  id="remember"
+                  aria-describedby="remember"
+                  type="checkbox"
+                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-800 dark:ring-offset-gray-800"
+                  required=""
+                />
+              </div>
+              <div class="ml-3 text-sm">
+                <label
+                  for="remember"
+                  class="text-gray-500 dark:text-gray-300"
+                >
+                  Remember me
+                </label>
+              </div>
             </div>
-     
-            <form class="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label
-                  for="email"
-                  class="block mb-2 text-lg font-light text-black dark:text-white text-center"
-                >
-                  Mobile Number
-                </label>
-                <input
-                  type="email"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  name="email"
-                  id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder-center"
-                  placeholder="254711438911"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  for="password"
-                  class="block mb-2 text-lg font-light text-gray-900 dark:text-white text-center"
-                >
-                  Pin
-                </label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  name="password"
-                  id="password"
-                  placeholder="••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div class="flex items-center justify-between">
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
-                    />
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label
-                      for="remember"
-                      class="text-gray-500 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-                <a
-                  href="#"
-                  class="text-sm font-thin hover:underline dark:text-primary-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
+            <a
+              href="#"
+              class="text-sm font-thin hover:underline dark:text-primary-500 mt-2 sm:mt-0"
+            >
+              Forgot password?
+            </a>
+          </div>
+
               <button
 
                 type="submit"
