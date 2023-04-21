a7490de0edbbaa841310aba1b2f2c58b068fee27
vsidera <siderravictor@gmail.com>
2023-04-21 14:39:22 +0300

    change 11

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>      
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+   
 
         <Routes>
           <Route path='/login' exact element={<Login/>} />
