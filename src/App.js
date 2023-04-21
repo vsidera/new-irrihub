71e340cb4433f911e1d536d2d16dbc91f6034411
vsidera <siderravictor@gmail.com>
2023-04-21 14:35:13 +0300

    change 10

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>      
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+
         <Routes>
           <Route path='/login' exact element={<Login/>} />
           {/* <Route path="my-devices/:id" element={<Home/>} /> */}
