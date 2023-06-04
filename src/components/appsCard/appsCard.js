1199d0fc77ecda4cc4bc70a3b930290c22dddf17
vsidera <siderravictor@gmail.com>
2023-06-04 07:38:57 +0300

    add names of devices

--- src/components/appsCard/appsCard.js
+++ src/components/appsCard/appsCard.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-function AppsCard({ imei_code}) {
+function AppsCard({ imei_code,device_type_id}) {
   return (
 
       <div>
       <h2 className="text-lg text-blue-800">
-        {/* {device_type_id === 1 && "SMART HOME"}
-        {device_type_id === 2 && "SMART FARM"}
-        {device_type_id === 3 && "WATER PAN"} */}
+        
         {imei_code}
       </h2>
+      <h4>
+      {device_type_id === 0 && "SMART HOME"}
+        {device_type_id === 2 && "SMART FARM"}
+        {device_type_id === 3 && "WATER PAN"} 
+      </h4>
 
       </div>
 
