1c55353480d6d1c97db62162f1a74cdc4aa886cd
vsidera <siderravictor@gmail.com>
2023-06-15 15:59:41 +0300

    add explore button

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -202,9 +202,20 @@ const Login = () => {
                   href="#"
                   class="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
-                  Sign up
+                  Explore
                 </a>
               </p>
+              <button
+                type="submit"
+                class="w-full text-white bg-green-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    
+                // onClick={(e) => {
+                //   handleLogin(e);
+                //   setIsButtonClicked(true);
+                // }}
+              >
+               Explore
+              </button>
             </form>
           </div>
         </div>
